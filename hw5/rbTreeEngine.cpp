a19e36cd5162e36222100e2c4e83498e76d33168
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-05-14 00:08:29 +0800

    rb tree

---
+++ hw5/rbTreeEngine.cpp
@@ -0,0 +1,1 @@
+#include "RbTreeEngine.h"
---
+++ hw5/rbTreeEngine.h
@@ -0,0 +1,20 @@
+#define RBTREE_ENGINE_INCLUDED
+#ifdef RBTREE_ENGINE_INCLUDED
+
+#include <map>
+#include <string>
+#include <vector>
+#include "BaseEngine.h"
+
+class RbTreeEngine : public BaseEngine{
+	private:
+		std::multimap<std::string,int> db;
+	public:
+		RbTreeEngine();
+		RbTreeEngine(int reserverdSize);
+		~RbTreeEngine();
+		void insert(const int & ithLine,const std::string & word);
+		bool search(const std::vector<std::string> & search , std::vector<std::string> & result);
+};
+
+#endif
